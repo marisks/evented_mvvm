5842fb6b6959ce4e94bfde546415385035c728ca
Māris Krivtežs <marisks@gmail.com>
2015-05-06 23:36:17 +0300

    Extracted UI logic into controller

--- src/MainWindow.xaml.fs
+++ src/MainWindow.xaml.fs
@@ -10,34 +10,33 @@ type MainView = XAML<"MainWindow.xaml", true>
 type Score = {
     ScoreA: int
     ScoreB: int
-}
+} with 
+    static member zero = {ScoreA = 0; ScoreB = 0}
+
 
 type ScoringEvent = IncA | DecA | IncB | DecB | New
 
-type MainViewModel() as self = 
+type Controller = Score -> ScoringEvent -> Score
+
+
+type MainViewModel(controller : Controller) as self = 
     inherit EventViewModelBase<ScoringEvent>()
 
-    let defaultScore = { ScoreA = 0; ScoreB = 0}
-    let mutable score = defaultScore
+    let mutable score = Score.zero
 
     let scoreA = self.Factory.Backing(<@ self.ScoreA @>, "00")
     let scoreB = self.Factory.Backing(<@ self.ScoreB @>, "00")
 
-    let updateScore () =
+    let updateView () =
         self.ScoreA <- score.ScoreA.ToString("D2")
         self.ScoreB <- score.ScoreB.ToString("D2")
 
     let eventHandler ev =
-        match ev with
-        | IncA -> score <- {score with ScoreA = score.ScoreA + 1}
-        | DecA -> score <- {score with ScoreA = score.ScoreA - 1}
-        | IncB -> score <- {score with ScoreB = score.ScoreB + 1}
-        | DecB -> score <- {score with ScoreB = score.ScoreB + 1}
-        | New -> score <- defaultScore
+        score <- controller score ev
 
     do
         self.EventStream
-        |> Observable.subscribe (eventHandler >> updateScore)
+        |> Observable.subscribe (eventHandler >> updateView)
         |> ignore
 
     member self.IncA = self.Factory.EventValueCommand(IncA)
@@ -50,3 +49,18 @@ type MainViewModel() as self =
                         and set value = scoreA.Value <- value
     member self.ScoreB with get() = scoreB.Value 
                         and set value = scoreB.Value <- value
+
+
+module Handling = 
+
+    let controller score ev =
+       match ev with
+        | IncA -> {score with ScoreA = score.ScoreA + 1}
+        | DecA -> {score with ScoreA = score.ScoreA - 1}
+        | IncB -> {score with ScoreB = score.ScoreB + 1}
+        | DecB -> {score with ScoreB = score.ScoreB - 1}
+        | New -> Score.zero 
+
+
+type CompositionRoot() =
+    member x.ViewModel with get() = MainViewModel(Handling.controller)
